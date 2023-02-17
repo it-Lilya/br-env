756d2dbd8e3014befaef84837c51d61fc2521432
It_Lilya <ihneva@bk.ru>
2023-02-17 21:36:20 +0300

    first

--- env/webpack.dev.js
+++ env/webpack.dev.js
@@ -1,22 +1,22 @@
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+import { HotModuleReplacementPlugin } from "webpack";
+import { merge } from "webpack-merge";
+import common from "./webpack.common";
 
-module.exports = merge(common, {
+export default merge(common, {
   // Set the mode to development or production
-  mode: 'development',
+  mode: "development",
   // Control how source maps are generated
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
 
   // Spin up a server for quick development
   devServer: {
     historyApiFallback: true,
     open: true,
-    compress: true
+    compress: true,
   },
 
   plugins: [
     // Only update what has changed on hot reload
-    new webpack.HotModuleReplacementPlugin(),
+    new HotModuleReplacementPlugin(),
   ],
 });
